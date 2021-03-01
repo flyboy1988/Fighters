50d95a83c0ae53fb722a81c43e5914345d65b739
flyboy <ali.aslani1367@gmail.com>
2021-03-01 18:49:07 +0330

    remove swipe2delete

--- swipe2delete/src/main/java/com/agilie/swipe2delete/interfaces/IUndoClickListener.kt
+++
@@ -1,5 +0,0 @@
-package com.flyboy.fighters.swipe2delete.interfaces
-
-interface IUndoClickListener<K> {
-    fun onUndo(key: K)
-}
--- swipe2delete/src/main/java/com/agilie/swipe2delete/interfaces/ItemSwipeListener.kt
+++
@@ -1,7 +0,0 @@
-package com.flyboy.fighters.swipe2delete.interfaces
-
-interface ItemSwipeListener<K> {
-    fun onItemSwiped(viewHolder: ISwipeToDeleteHolder<K>, swipeDir: Int)
-
-    fun clearView(viewHolder: ISwipeToDeleteHolder<K>)
-}
